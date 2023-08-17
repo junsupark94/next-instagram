d66e7064b26f5d14077b21aff41e8767b0746044
Junsu Park <junsupark94@gmail.com>
2023-08-16 21:41:54 -0700

    Made the links in Footer work

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,14 @@ import React from "react";
 type FooterProps = {};
 
 const links = [
-  "About",
-  "Help",
-  "Press",
-  "API",
-  "Jobs",
-  "Privacy",
-  "Terms",
-  "Locations",
+  {text: "About", src: "https://about.instagram.com/"},
+  {text: "Help", src: "https://help.instagram.com/"},
+  {text: "Press", src: "https://about.instagram.com/blog"},
+  {text: "API", src: "https://developers.facebook.com/docs/instagram"},
+  {text: "Jobs", src: "https://about.instagram.com/about-us/careers"},
+  {text: "Privacy", src: "https://privacycenter.instagram.com/policy/?entry_point=ig_help_center_data_policy_redirect"},
+  {text: "Terms", src: "https://help.instagram.com/581066165581870/"},
+  {text: "Locations", src: "https://www.instagram.com/explore/locations/"},
 ];
 
 const Footer: React.FC<FooterProps> = () => {
@@ -19,9 +19,9 @@ const Footer: React.FC<FooterProps> = () => {
     <footer className="text-xs text-[#737373]">
       <ul className="flex flex-wrap">
         {links.map((link) => (
-          <li key={link} className="after:content-['\00B7'] after:mx-[3px]">
-            <Link href="/" className="hover:underline">
-              {link}
+          <li key={link.text} className="after:content-['\00B7'] after:mx-[3px]">
+            <Link href={link.src} target="_blank" className="hover:underline">
+              {link.text}
             </Link>
           </li>
         ))}
@@ -29,7 +29,7 @@ const Footer: React.FC<FooterProps> = () => {
           <span>Languages</span>
         </li>
         <li>
-          <Link href="/" className="hover:underline">
+          <Link href="https://about.meta.com/technologies/meta-verified/" target="_blank" className="hover:underline">
             Meta Verified
           </Link>
         </li>
