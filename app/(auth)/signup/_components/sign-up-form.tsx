ecf2e65f7c64cf7680f0a3343b2dd76baeb6ddc3
Junsu Park <junsupark94@gmail.com>
2023-10-04 23:33:57 -0700

    Refactored Sign Up Page to also be server side so redirect works without rendering

---
+++ app/(auth)/signup/_components/sign-up-form.tsx
@@ -0,0 +1,116 @@
+'use client'
+
+import FormInput from "@/components/Auth/form-input";
+import PasswordInput from "@/components/Auth/password-input";
+import { cn } from "@/lib/utils";
+import { SignUpType, signUpSchema } from "@/lib/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import router from "next/router";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const SignUpForm = () => {
+  const [serverError, setServerError] = useState("");
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpType>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      login: "1112223333",
+      password: "123456jP!",
+      username: "junsumoney",
+      fullname: "Junsu Park",
+    },
+    mode: "onBlur",
+  });
+
+  const onSubmit = async (formData: SignUpType) => {
+    setServerError("");
+    try {
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        body: JSON.stringify(formData),
+      });
+      if (!response.ok) {
+        const serverError = await response.text();
+        setServerError(serverError);
+      } else {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log("WTF");
+      console.error(error);
+      setServerError("Something wen't wrong, please try again!");
+    }
+  };
+  return (
+    <>
+      {" "}
+      {serverError !== "" && (
+        <p className="text-xs text-red-500">{serverError}</p>
+      )}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex w-full flex-col items-center gap-y-[6px]"
+      >
+        <FormInput
+          watch={watch}
+          register={register}
+          field="login"
+          label="Mobile number or Email"
+        />
+        {errors.login && (
+          <p className="text-xs text-red-500">{errors.login.message}</p>
+        )}
+        <FormInput
+          watch={watch}
+          register={register}
+          field="fullname"
+          label="Full Name"
+        />
+        {errors.fullname && (
+          <p className="text-xs text-red-500">{errors.fullname.message}</p>
+        )}
+        <FormInput
+          watch={watch}
+          register={register}
+          field="username"
+          label="Username"
+        />
+        {errors.username && (
+          <p className="text-xs text-red-500">{errors.username.message}</p>
+        )}
+        <PasswordInput watch={watch} register={register} />
+        {errors.password && (
+          <p className="text-xs text-red-500">{errors.password.message}</p>
+        )}
+        <p className="my-2 text-center text-xs text-neutral-500">
+          This would be where legalese terms go but this is just a clone
+          project.
+          <Link
+            href="www.linkedin.com/in/junsupark-swe"
+            className="text-blue-600"
+          >
+            {" "}
+            Learn more.
+          </Link>
+        </p>
+        <button
+          disabled={isSubmitting}
+          className={cn(
+            "mb-10 w-full rounded-md bg-sky-400 py-2 text-sm font-semibold text-white",
+            isSubmitting && "brightness-75",
+          )}
+        >
+          {isSubmitting ? "Submitting" : "Sign up"}
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default SignUpForm;
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,57 +1,17 @@
-"use client";
 import InstagramLogo from "@/Icons/InstagramLogo";
 import FacebookIcon from "@/public/facebookIcon.png";
 import Image from "next/image";
-
-import { cn } from "@/lib/utils";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-
-import { useState } from "react";
 import Link from "next/link";
-import { SignUpType, signUpSchema } from "@/lib/zod";
-import { redirect, useRouter } from "next/navigation";
-import FormInput from "@/components/Auth/form-input";
-import PasswordInput from "@/components/Auth/password-input";
+import SignUpForm from "./_components/sign-up-form";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const SignUpPage = () => {
-  const router = useRouter();
-  const [serverError, setServerError] = useState("");
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors, isSubmitting },
-  } = useForm<SignUpType>({
-    resolver: zodResolver(signUpSchema),
-    defaultValues: {
-      login: "1112223333",
-      password: "123456jP!",
-      username: "junsumoney",
-      fullname: "Junsu Park",
-    },
-    mode: "onBlur",
-  });
+  const isAuth = cookies().get("JUNSU-AUTH")?.value === "some_secret";
 
-  const onSubmit = async (formData: SignUpType) => {
-    setServerError("");
-    try {
-      const response = await fetch("/api/auth/signup", {
-        method: "POST",
-        body: JSON.stringify(formData),
-      });
-      if (!response.ok) {
-        const serverError = await response.text();
-        setServerError(serverError);
-      } else {
-        router.push("/");
-      }
-    } catch (error) {
-      console.log("WTF");
-      console.error(error);
-      setServerError("Something wen't wrong, please try again!");
-    }
-  };
+  if (isAuth) {
+    redirect("/");
+  }
 
   return (
     <div className="flex h-full items-center justify-center">
@@ -74,65 +34,7 @@ const SignUpPage = () => {
             </div>
             <div className="h-[1px] grow bg-zinc-200" />
           </div>
-          {serverError !== "" && (
-            <p className="text-xs text-red-500">{serverError}</p>
-          )}
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex w-full flex-col items-center gap-y-[6px]"
-          >
-            <FormInput
-              watch={watch}
-              register={register}
-              field="login"
-              label="Mobile number or Email"
-            />
-            {errors.login && (
-              <p className="text-xs text-red-500">{errors.login.message}</p>
-            )}
-            <FormInput
-              watch={watch}
-              register={register}
-              field="fullname"
-              label="Full Name"
-            />
-            {errors.fullname && (
-              <p className="text-xs text-red-500">{errors.fullname.message}</p>
-            )}
-            <FormInput
-              watch={watch}
-              register={register}
-              field="username"
-              label="Username"
-            />
-            {errors.username && (
-              <p className="text-xs text-red-500">{errors.username.message}</p>
-            )}
-            <PasswordInput watch={watch} register={register} />
-            {errors.password && (
-              <p className="text-xs text-red-500">{errors.password.message}</p>
-            )}
-            <p className="my-2 text-center text-xs text-neutral-500">
-              This would be where legalese terms go but this is just a clone
-              project.
-              <Link
-                href="www.linkedin.com/in/junsupark-swe"
-                className="text-blue-600"
-              >
-                {" "}
-                Learn more.
-              </Link>
-            </p>
-            <button
-              disabled={isSubmitting}
-              className={cn(
-                "mb-10 w-full rounded-md bg-sky-400 py-2 text-sm font-semibold text-white",
-                isSubmitting && "brightness-75",
-              )}
-            >
-              {isSubmitting ? "Submitting" : "Sign up"}
-            </button>
-          </form>
+          <SignUpForm />
         </article>
         <article className="my-2 w-full border border-zinc-300 py-4 text-center text-sm">
           <p>
